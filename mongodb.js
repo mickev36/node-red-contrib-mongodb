2ce2656c637b1deb82049553f16b8baca5a1cd7a
mickev36 <kevinmichelet91@gmail.com>
2018-10-26 13:29:01 +0200

    First working implementation

--- mongodb.js
+++ mongodb.js
@@ -1,11 +1,11 @@
 
 module.exports = function(RED) {
     "use strict";
-    var mongo = require('mongodb');
-    var ObjectID = require('mongodb').ObjectID;
-    var MongoClient = mongo.MongoClient;
+    const mongo = require('mongodb');
+    const ObjectID = require('mongodb').ObjectID;
+    const MongoClient = mongo.MongoClient;
 
-    function MongoNode(n) {
+    function MongoConfigNode(n) {
         RED.nodes.createNode(this,n);
         this.hostname = n.hostname;
         this.port = n.port;
@@ -21,7 +21,7 @@ module.exports = function(RED) {
         this.url = url;
     }
 
-    RED.nodes.registerType("mongodb",MongoNode,{
+    RED.nodes.registerType("mongodb-config",MongoConfigNode,{
         credentials: {
             user: {type:"text"},
             password: {type: "password"}
@@ -35,135 +35,8 @@ module.exports = function(RED) {
         return selector;
     }
 
-    function MongoOutNode(n) {
-        RED.nodes.createNode(this,n);
-        this.collection = n.collection;
-        this.mongodb = n.mongodb;
-        this.payonly = n.payonly || false;
-        this.upsert = n.upsert || false;
-        this.multi = n.multi || false;
-        this.operation = n.operation;
-        this.mongoConfig = RED.nodes.getNode(this.mongodb);
-        this.status({fill:"grey",shape:"ring",text:RED._("mongodbstatus.connecting")});
-        var node = this;
-        var noerror = true;
 
-        var connectToDB = function() {
-            MongoClient.connect(node.mongoConfig.url, function(err, db) {
-                if (err) {
-                    node.status({fill:"red",shape:"ring",text:RED._("mongodb.status.error")});
-                    if (noerror) { node.error(err); }
-                    noerror = false;
-                    node.tout = setTimeout(connectToDB, 10000);
-                }
-                else {
-                    node.status({fill:"green",shape:"dot",text:RED._("mongodb.status.connected")});
-                    node.clientDb = db;
-                    noerror = true;
-                    var coll;
-                    if (node.collection) {
-                        coll = db.collection(node.collection);
-                    }
-                    node.on("input",function(msg) {
-                        if (!node.collection) {
-                            if (msg.collection) {
-                                coll = db.collection(msg.collection);
-                            }
-                            else {
-                                node.error(RED._("mongodb.errors.nocollection"),msg);
-                                return;
-                            }
-                        }
-                        delete msg._topic;
-                        delete msg.collection;
-                        if (node.operation === "store") {
-                            if (node.payonly) {
-                                if (typeof msg.payload !== "object") {
-                                    msg.payload = {"payload": msg.payload};
-                                }
-                                if (msg.hasOwnProperty("_id") && !msg.payload.hasOwnProperty("_id")) {
-                                    msg.payload._id = msg._id;
-                                }
-                                coll.save(msg.payload,function(err, item) {
-                                    if (err) {
-                                        node.error(err,msg);
-                                    }
-                                });
-                            }
-                            else {
-                                coll.save(msg,function(err, item) {
-                                    if (err) {
-                                        node.error(err,msg);
-                                    }
-                                });
-                            }
-                        }
-                        else if (node.operation === "insert") {
-                            if (node.payonly) {
-                                if (typeof msg.payload !== "object") {
-                                    msg.payload = {"payload": msg.payload};
-                                }
-                                if (msg.hasOwnProperty("_id") && !msg.payload.hasOwnProperty("_id")) {
-                                    msg.payload._id = msg._id;
-                                }
-                                coll.insert(msg.payload, function(err, item) {
-                                    if (err) {
-                                        node.error(err,msg);
-                                    }
-                                });
-                            }
-                            else {
-                                coll.insert(msg, function(err,item) {
-                                    if (err) {
-                                        node.error(err,msg);
-                                    }
-                                });
-                            }
-                        }
-                        else if (node.operation === "update") {
-                            if (typeof msg.payload !== "object") {
-                                msg.payload = {"payload": msg.payload};
-                            }
-                            var query = msg.query || {};
-                            var payload = msg.payload || {};
-                            var options = {
-                                upsert: node.upsert,
-                                multi: node.multi
-                            };
-                            if (ObjectID.isValid(msg.query._id)) {
-                                msg.query._id = new ObjectID(msg.query._id);
-                            }
-                            coll.update(query, payload, options, function(err, item) {
-                                if (err) {
-                                    node.error(err,msg);
-                                }
-                            });
-                        }
-                        else if (node.operation === "delete") {
-                            coll.remove(msg.payload, function(err, items) {
-                                if (err) {
-                                    node.error(err,msg);
-                                }
-                            });
-                        }
-                    });
-                }
-            });
-        }
-
-        if (node.mongoConfig) { connectToDB(); }
-        else { node.error(RED._("mongodb.errors.missingconfig")); }
-
-        node.on("close", function() {
-            node.status({});
-            if (node.tout) { clearTimeout(node.tout); }
-            if (node.clientDb) { node.clientDb.close(); }
-        });
-    }
-    RED.nodes.registerType("mongodb out",MongoOutNode);
-
-
-    function MongoInNode(n) {
+    function MongoNode(n) {
         RED.nodes.createNode(this,n);
         this.collection = n.collection;
         this.mongodb = n.mongodb;
@@ -185,7 +58,10 @@ module.exports = function(RED) {
                     node.status({fill:"green",shape:"dot",text:RED._("mongodb.status.connected")});
                     node.clientDb = db;
                     noerror = true;
-                    var coll;
+					var coll;
+					if (node.collection) {
+                        coll = db.collection(node.collection);
+                    }
                     node.on("input", function(msg) {
                         if (!node.collection) {
                             if (msg.collection) {
@@ -253,6 +129,74 @@ module.exports = function(RED) {
                                     node.send(msg);
                                 }
                             });
+						}
+
+						else if (node.operation === "store") {
+								if (typeof msg.payload !== "object") {
+									msg.payload = {"payload": msg.payload};
+								}
+								if (msg.hasOwnProperty("_id") && !msg.payload.hasOwnProperty("_id")) {
+									msg.payload._id = msg._id;
+								}
+                                coll.save(msg.payload,function(err, item) {
+                                    if (err) {
+                                        node.error(err,msg);
+									}
+									else {
+										msg.payload = item;
+										node.send(msg);
+									}
+                                });
+                        }
+                        else if (node.operation === "insert") {
+								if (typeof msg.payload !== "object") {
+									msg.payload = {"payload": msg.payload};
+								}
+								if (msg.hasOwnProperty("_id") && !msg.payload.hasOwnProperty("_id")) {
+									msg.payload._id = msg._id;
+								}
+                                coll.insert(msg.payload, function(err,item) {
+                                    if (err) {
+                                        node.error(err,msg);
+                                    } else {
+										msg.payload = item;
+										node.send(msg);
+									}
+                                });
+                        }
+                        else if (node.operation === "update") {
+                            if (typeof msg.payload !== "object") {
+                                msg.payload = {"payload": msg.payload};
+                            }
+                            var query = msg.query || {};
+                            var payload = msg.payload || {};
+                            var options = {
+                                upsert: node.upsert,
+                                multi: node.multi
+                            };
+                            if (ObjectID.isValid(msg.query._id)) {
+                                msg.query._id = new ObjectID(msg.query._id);
+                            }
+                            coll.update(query, payload, options, function(err, item) {
+                                if (err) {
+                                    node.error(err,msg);
+								}
+								else {
+									msg.payload = items;
+									node.send(msg);
+								}
+                            });
+                        }
+                        else if (node.operation === "delete") {
+                            coll.remove(msg.payload, function(err, items) {
+                                if (err) {
+                                    node.error(err,msg);
+								}
+								else {
+									msg.payload = items;
+									node.send(msg);
+								}
+                            });
                         }
                     });
                 }
@@ -268,5 +212,5 @@ module.exports = function(RED) {
             if (node.clientDb) { node.clientDb.close(); }
         });
     }
-    RED.nodes.registerType("mongodb in",MongoInNode);
+    RED.nodes.registerType("mongodb-node",MongoNode);
 }
